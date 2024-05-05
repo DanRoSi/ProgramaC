792e88c4c3c8c2bb7406b4bf14b13fd81b6a9e29
felipe12l <andres.luna01@uptc.edu.co>
2024-05-04 20:20:38 -0500

    fixed method, now can start from specified index

--- main.c
+++ main.c
@@ -1,28 +1,40 @@
-#include<string.h>
 #include <stdio.h>
-int equals(char a[],char b[]);
-int findingOcurrence(char str[],char sub[],int init);
-int main(){
-    char a[]="cola";
-    char b[]="holassss";
+#include <string.h>
 
-    printf("%d",findingOcurrence(&a,&b,2));
-    return 0;
-}
-int findingOcurrence(char str[],char sub[],int init) {
-    if (init < 0) {
+int sub_str(const char *str, const char *sub, int init) {
+    int len = strlen(str);
+    int sublen = strlen(sub);
+    if (init < 0 || init >= strlen) {
         return -1;
     }
-    if (strlen(str) < strlen(sub)) {
-        return -2;
-    }
-    char *resultado = strstr(str, sub);
 
-    if (resultado != NULL) {
-        int pos=resultado - str;
-        return pos;
-    } else {
-        return 0;
+    int i, j;
+    for (i = init; i <= len - sublen; i++) {
+        if (str[i] == sub[0]) {
+            for (j = 1; j < sublen; j++) {
+                if (str[i + j] != sub[j]) {
+                    break; 
+                }
+            }
+            if (j == sublen) {
+                return i; 
+            }
+        }
     }
+    return 0;
 }
 
+int main() {
+    const char *str = "Hola, esto es un ejemplo.";
+    const char *sub = "ejemplo";
+    int init = 1;
+
+    int indice = sub_str(str, sub, init);
+    if (indice != 0) {
+        printf("La cadena '%s' fue encontrada en el índice %d.\n", sub, indice);
+    } else {
+        printf("La cadena'%s' no fue encontrada.\n", sub);
+    }
+
+    return 0;
+}
